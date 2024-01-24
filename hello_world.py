38c8ffedd02a6bc7708f912fed2b0d7fb1cdb1a5
Sophia Miller <sopmill@siue.edu>
2024-01-24 13:28:08 -0600

    I added a few lines

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,10 @@
 print ("Hello world!")
 print ("I haven't used python in three years")
+guess = input ("Guess my favorite color:")
+
+if (guess != orange | Orange){
+    print ("Nope, it's orange")
+}
+else{
+    print ("Yes! You got it!")
+}
